7ff0f2ff124d57ac51443b3a0aa45e8e4d285c50
Arina Godman <146754998+ArinaGodman@users.noreply.github.com>
2024-03-11 14:27:55 +0100

    Final

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import cv2
+
+from predicction import make_prediction
 #from prediction import predict
 
 # Function to display a big prediction number
@@ -18,9 +21,12 @@ uploaded_file = st.file_uploader('Upload your photo', type=['jpg', 'jpeg', 'png'
 if st.button('Challenge Model'):
     if uploaded_file is not None:
         # Add your model prediction logic here
-        predicted_number = make_prediction(uploaded_file)  # Replace with the actual predicted number
+        image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+        predicted_number = make_prediction(image)  # Replace with the actual predicted number
 
         # Display the prediction number
+         # Display the original and processed images
+        st.image([image], caption=['Original Image'], width=300)
         display_prediction_number(predicted_number)
     else:
         st.warning('Please upload a photo before challenging the model.')
