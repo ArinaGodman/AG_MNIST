d6ae36667821f3d50722f5d79565503ff329b463
Arina Godman <146754998+ArinaGodman@users.noreply.github.com>
2024-03-14 16:42:09 +0100

    Working version of my app!!!

--- app.py
+++ app.py
@@ -1,33 +1,69 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import cv2
+from streamlit_drawable_canvas import st_canvas
 
 from predicction import make_prediction
-#from prediction import predict
 
-# Function to display a big prediction number
+
 def display_prediction_number(predicted_number):
-    st.write(f'## Prediction: {predicted_number}')
+    st.write(f'<h2 style="color:#333333;">Prediction: {predicted_number[0]}</h2>', unsafe_allow_html=True)
 
-# Main Streamlit app
-st.title('Reading Numbers')
-st.markdown('Model to read a number from your photo')
+st.title('Arinas ML-model that can read your numbers!')
+st.write('<p style="font-size:20px; color:#333333;">Here you can upload or draw a number and challenge my model to predict what number it is seeing.</p>', unsafe_allow_html=True)
+st.write('<p style="font-size:20px; color:#333333;">Numbers should be drawn in the middle on the canvas/your picture with a black thick pen.</p>', unsafe_allow_html=True)
+st.write('<p style="font-size:20px; color:#333333;">Remember that it is a simple Machine Learing algorithm, so, do not expect magic from it , hehe</p>', unsafe_allow_html=True)
 
-# File uploader
-uploaded_file = st.file_uploader('Upload your photo', type=['jpg', 'jpeg', 'png'])
+drawing_mode = st.checkbox('Enable Drawing')
 
-# Button to challenge the model
-if st.button('Challenge Model'):
-    if uploaded_file is not None:
-        # Add your model prediction logic here
-        image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
-        predicted_number = make_prediction(image)  # Replace with the actual predicted number
+SIZE = 200
 
-        # Display the prediction number
-         # Display the original and processed images
-        st.image([image], caption=['Original Image'], width=300)
-        display_prediction_number(predicted_number)
-    else:
-        st.warning('Please upload a photo before challenging the model.')
+col1, col2 = st.columns(2)
+if drawing_mode:
+    canvas_result = st_canvas(
+        stroke_width=20,  
+        stroke_color="white",  
+        background_color="black",  
+        update_streamlit=True,  
+        height=SIZE, 
+        width=SIZE,  
+        drawing_mode="freedraw",  
+    )
 
+    
+    if canvas_result.image_data is not None:
+        img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+        rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+        st.write('Model Input')
+        st.image(rescaled)
+
+    if st.button('Predict'):
+        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        prediction = make_prediction(gray_image)
+        display_prediction_number(prediction)
+
+else:
+
+    uploaded_file = st.file_uploader('Upload your photo', type=['jpg', 'jpeg', 'png'])
+
+    
+    if st.button('Predict'):
+        if uploaded_file is not None:
+            
+            image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            resized_image = cv2.resize(gray_image, (28, 28))
+            normalized_image = resized_image / 255.0
+            inverted_image = (1 - normalized_image) * 255.0
+            mean_pixel_value = np.mean(inverted_image)
+            inverted_image[inverted_image <= mean_pixel_value] = 0  
+
+            
+            prediction = make_prediction(inverted_image)
+            st.image(image, caption='Original Image', use_column_width=True)
+            
+            digit = prediction[0]
+            display_prediction_number(prediction)
+
+        else:
+            st.warning('Please upload a photo before challenging the model.')
--- predicction.py
+++ predicction.py
@@ -6,31 +6,11 @@ import os
 from sklearn.preprocessing import StandardScaler
 
 def preprocess_image(image, std_scaler_path='std_scaler.sav'):
-    # Convert to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    flattened_img = image.flatten()
+    reshaped_img_2d = flattened_img.reshape(1, -1)  
 
-    # Resize the image to 28x28 pixels
-    resized_image = cv2.resize(gray_image, (28, 28))
-
-    # Normalize pixel values to the range [0, 1]
-    normalized_image = resized_image / 255.0
-
-    inverted_image = (1 - normalized_image) * 255.0
-
-    mean_pixel_value = np.mean(inverted_image)
-    inverted_image[inverted_image <= mean_pixel_value] = 0 # Set pixels below mean to white
-    #inverted_image[inverted_image > mean_pixel_value] = 255
-
-    # Flatten to a 1D array
-    flattened_img = inverted_image.flatten()
-
-    # Reshape to a 2D array for StandardScaler
-    reshaped_img_2d = flattened_img.reshape(1, -1)  # Change the shape to (1, 784)
-
-    # Load StandardScaler
+   
     std_scaler = joblib.load(std_scaler_path)
-
-    # Apply StandardScaler without considering feature names
     img_ready = std_scaler.transform(reshaped_img_2d)
 
     return img_ready.flatten()
@@ -41,8 +21,6 @@ def make_prediction(image):
     model_path = os.path.join(current_dir, 'svc_model.sav')
     model = joblib.load(model_path)
     
-    # Flatten the image array if needed (depends on the model input)
-    # predicted_number = model.predict([image_pp.flatten()])
     predicted_number = model.predict([image_pp])
     
-    return predicted_number[0]  # Get the first element from the prediction array
+    return predicted_number[0] 
